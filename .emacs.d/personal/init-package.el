e83d0ab7294c987e669e0553f57dd2aa750f53c0
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 15:34:15 +0530

    init-package.el: Emacs package configurations

---
+++ .emacs.d/personal/init-package.el
@@ -0,0 +1,25 @@
+;; Emacs supports packages that can be installed on the go.
+
+(require 'package)
+
+;; The package list to be installed
+(setq package-list
+      '(yasnippet))
+
+;; Repositories to be used to search and install packages.
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa-stable" . "https://stable.melpa.org/packages/")))
+
+(package-initialize)
+
+;; Install packages that are not installed
+(let ((refreshed nil))
+  (dolist (pkg package-list)
+    (when (not (package-installed-p pkg))
+      (unless refreshed
+        (package-refresh-contents)
+        (setq refreshed t))
+      (package-install pkg))))
+
+(provide 'init-package)
