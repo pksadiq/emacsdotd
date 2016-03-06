102e7c102829932407bf5c7b31b84f679f45e9ac
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 12:05:21 +0530

    init-common.el: Populate with more settings

--- .emacs.d/personal/init-common.el
+++ .emacs.d/personal/init-common.el
@@ -4,6 +4,23 @@
 ;; Don't show startup screen
 (setq inhibit-startup-message t)
 
+;; Let's type y/n instead of yes/no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Auto close bracket (", [, (, {, etc.).
+(electric-pair-mode 1)
+
+;; Highlight matching parenthesis
+(show-paren-mode 1)
+
+;; Show column number -- The number of characters from the beginning
+;; of the current line up to the cursor
+(column-number-mode 1)
+
+;; No TABs, please. Let everything be space of 2
+(setq tab-width 2
+      indent-tabs-mode nil)
+
 ;; Let this file provide a feature named 'init-common'.
 ;; Use `require' function to load to current Emacs session.
 (provide 'init-common)
