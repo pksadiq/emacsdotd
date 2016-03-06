444783c2292a5eb6adce67ab10caef35e1c3b1b6
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 11:37:57 +0530

    init-common.el: Common Emacs configuration

---
+++ .emacs.d/personal/init-common.el
@@ -0,0 +1,9 @@
+;; Configurations that are common to more than one Emacs mode.
+;; Or configurations that doesn't fit anywhere else.
+
+;; Don't show startup screen
+(setq inhibit-splash-screen t)
+
+;; Let this file provide a feature named 'init-common'.
+;; Use `require' function to load to current Emacs session.
+(provide 'init-common)
