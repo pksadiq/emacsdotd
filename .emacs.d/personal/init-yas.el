f37e6be448afa55ccffc5757c9d955a566065071
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 16:23:30 +0530

    init-yas.el: Load snippets only from custom dir

--- .emacs.d/personal/init-yas.el
+++ .emacs.d/personal/init-yas.el
@@ -2,6 +2,9 @@
 
 (require 'yasnippet)
 
+;; Don't use the default snippets. Use the following instead.
+(setq yas-snippet-dirs '("~/.emacs.d/personal/snippets"))
+
 ;; Enable yasnippet everywhere it supports
 (yas-global-mode 1)
 
