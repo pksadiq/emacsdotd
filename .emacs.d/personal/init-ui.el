fcfb3e59d811b4c20dfcc85d176e16c9dc867cc2
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 11:20:15 +0530

    init-ui.el: Elisp related to Emacs UI elements

---
+++ .emacs.d/personal/init-ui.el
@@ -0,0 +1,18 @@
+;; Configurations that relates to Emacs UI elements in general.
+;; Like menubar, toolbar, fringes, fonts, theme, etc.
+
+;; Fonts should already be installed before you can use it.
+(set-default-font "Inconsolata-16")
+
+;; Hide space consuming things from screen
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Disable Fringes, the gray bar of about 0.5 cm (default)
+;; on left and right of Emacs X11
+(set-fringe-mode '(0 . 0))
+
+;; Let this file provide a feature named 'init-ui'.
+;; Use `require' function to load to current Emacs session.
+(provide 'init-ui)
