0c4b974b51184c37137013e7417c58634f4c7c47
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 10:51:44 +0530

    init.el: New file

---
+++ .emacs.d/init.el
@@ -0,0 +1,9 @@
+;; `init.el': Helper script to load other files
+
+;; This script just loads other files. Nothing else.
+;; KISS?
+
+;; Assign one more path to be searched for elisp files.
+;; Note: No files shall be loaded from the path unless
+;; Explicitly said.
+(add-to-list 'load-path "~/.emacs.d/personal/")
