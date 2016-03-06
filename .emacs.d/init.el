9b1d522f8cd448f61644b7f520c9bf70512c3321
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 11:40:24 +0530

    init.el: Reduce even more comments

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,18 +1,15 @@
 ;; `init.el': Helper script to load other files
-
 ;; This script just loads other files. Nothing else. KISS?
 
 ;; Assign one more path to be searched for elisp files.
 (add-to-list 'load-path "~/.emacs.d/personal/")
 
+;; Search for the library 'init-common' in 'load-path' and load it.
 ;; Configures common Emacs settings.
 ;; Defined in: ~/.emacs.d/personal/init-common.el
 (load-library "init-common")
 (require 'init-common)
 
-;; Search for the library 'init-ui' in 'load-path'
-;; and load it.
 ;; Configures Emacs UI related elements.
-;; Defined in: ~/.emacs.d/personal/init-ui.el
 (load-library "init-ui")
 (require 'init-ui)
