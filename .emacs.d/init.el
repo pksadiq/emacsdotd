6effdb3e6ae74ecd7c07002c2a3457636614d119
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-06 11:25:55 +0530

    init.el: Load init-ui.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,3 +7,10 @@
 ;; Note: No files shall be loaded from the path unless
 ;; Explicitly said.
 (add-to-list 'load-path "~/.emacs.d/personal/")
+
+;; Search for the library 'init-ui' in 'load-path'
+;; and load it.
+;; Configures Emacs UI related elements.
+;; Defined in: ~/.emacs.d/personal/init-ui.el
+(load-library "init-ui")
+(require 'init-ui)
