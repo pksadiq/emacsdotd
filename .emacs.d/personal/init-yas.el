6923a40754d7e84462c997eb29d9598e531018c7
Mohammed Sadik <sadiq@sadiqpk.org>
2016-03-08 17:09:25 +0530

    init-yas.el: Don't enable yasnippet in term-mode

--- .emacs.d/personal/init-yas.el
+++ .emacs.d/personal/init-yas.el
@@ -5,6 +5,9 @@
 ;; Don't use the default snippets. Use the following instead.
 (setq yas-snippet-dirs '("~/.emacs.d/personal/snippets"))
 
+(add-hook 'term-mode-hook (lambda()
+			    (yas-minor-mode -1)))
+
 ;; Enable yasnippet everywhere it supports
 (yas-global-mode 1)
 
