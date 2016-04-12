468f6590cbcda2298031189eb919f752ec542c9f
Mohammed Sadik <sadiq@sadiqpk.org>
2016-04-12 12:42:00 +0530

    erc: tab complete the last nick

--- .emacs.d/personal/init-erc.el
+++ .emacs.d/personal/init-erc.el
@@ -3,4 +3,8 @@
 ;; Hide distracting info from `erc' IRC client
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
+(add-hook 'erc-mode-hook
+    '(lambda ()
+       (local-set-key (kbd "<tab>") 'erc-get-last-nick)))
+
 (provide 'init-erc)
