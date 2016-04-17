5837aaf84a8ec2fe45aeddc5d43df7de49a5f10b
Mohammed Sadik <sadiq@sadiqpk.org>
2016-04-17 06:36:27 +0530

    UI: set UI related confs only if ui available

--- .emacs.d/personal/init-ui.el
+++ .emacs.d/personal/init-ui.el
@@ -17,13 +17,13 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; Hide space consuming things from screen
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; Disable Fringes, the gray bar of about 0.5 cm (default)
 ;; on left and right of Emacs X11
-(set-fringe-mode '(0 . 0))
+(when (fboundp 'set-fringe-mode) (set-fringe-mode '(0 . 0)))
 
 ;; Let this file provide a feature named 'init-ui'.
 ;; Use `require' function to load to current Emacs session.
