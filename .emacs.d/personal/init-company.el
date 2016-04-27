90ae7018a3a943e4f2cc7ec128eeac10572ac95f
Mohammed Sadik <sadiq@sadiqpk.org>
2016-04-27 09:52:00 +0530

    company: complete early

--- .emacs.d/personal/init-company.el
+++ .emacs.d/personal/init-company.el
@@ -9,7 +9,7 @@
 (setq company-minimum-prefix-length 1)
 
 ;; Show completion faster
-(setq company-idle-delay 0.5)
+(setq company-idle-delay 0.3)
 
 ;; Show enumerated completion
 (setq company-show-numbers t)
