306b49e040a0e25783c97048acbfcade6a50d488
Mohammed Sadik <sadiq@sadiqpk.org>
2016-04-29 12:21:09 +0530

    yasnippet: 'f.s' snippet to create signal functions

---
+++ .emacs.d/personal/snippets/c-mode/f.s
@@ -0,0 +1,10 @@
+# -*- mode: snippet -*-
+# name: function for signals
+# key: f.s
+# --
+${1:static }${2:void}
+${3:`(or gnome-sig-defun "my_function")`} (${4:GApplication *app},
+      gpointer user_data)
+{
+  $0
+}
