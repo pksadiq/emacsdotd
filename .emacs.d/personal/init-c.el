bf698eea6af38860a1d6ed5eb07206a001be0968
Mohammed Sadik <sadiq@sadiqpk.org>
2016-05-05 11:21:53 +0530

    init-c: add more c macro declarations

--- .emacs.d/personal/init-c.el
+++ .emacs.d/personal/init-c.el
@@ -5,8 +5,11 @@
 ;; Multiline comment prefix
 (setq c-block-comment-prefix "*")
 
-(setq c-macro-names-with-semicolon '("G_BEGIN_DECLS" "G_DECLARE_FINAL_TYPE"
-                                     "G_END_DECLS"))
+(setq c-macro-names-with-semicolon
+      '("G_BEGIN_DECLS" "G_END_DECLS" "G_DECLARE_FINAL_TYPE"
+        "G_DECLARE_DERIVABLE_TYPE" "G_DECLARE_INTERFACE" "G_DEFINE_TYPE"
+        "G_DEFINE_TYPE_WITH_PRIVATE" "G_DEFINE_ABSTRACT_TYPE"
+        "G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE"))
 
 (defvar gnome-package nil)
 (make-variable-buffer-local 'gnome-package)
