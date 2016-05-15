a8d437f9a47df6f13ed41e2d0585830702bf12e4
Mohammed Sadik <sadiq@sadiqpk.org>
2016-05-15 07:04:39 +0530

    init: remove redundant code

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,80 +4,60 @@
 ;; Assign one more path to be searched for elisp files.
 (add-to-list 'load-path "~/.emacs.d/personal/")
 
-;; Search for the library 'init-common' in 'load-path' and load it.
 ;; Configures common Emacs settings.
 ;; Defined in: ~/.emacs.d/personal/init-common.el
-(load-library "init-common")
 (require 'init-common)
 
 ;; Configure repositories and install required packages
-(load-library "init-package")
 (require 'init-package)
 
 ;; Load C prgramming related user defined functions
-(load-library "init-c-defun")
 (require 'init-c-defun)
 
 ;; Load user defined functions
-(load-library "init-defun")
 (require 'init-defun)
 
 ;; Configures Common keyboard shortcuts
-(load-library "init-keys")
 (require 'init-keys)
 
 ;; A minor evil mode
-(load-library "less-evil-mode")
 (require 'less-evil-mode)
 
 ;; Configures Emacs UI related elements.
-(load-library "init-ui")
 (require 'init-ui)
 
 ;; Configure yasnippet snippets.
-(load-library "init-yas")
 (require 'init-yas)
 
 ;; Configure interactive do
-(load-library "init-ido")
 (require 'init-ido)
 
 ;; Configure avy-mode
-(load-library "init-avy")
 (require 'init-avy)
 
 ;; Configures `company-mode' 
-(load-library "init-company")
 (require 'init-company)
 
 ;; Configures cc-mode
-(load-library "init-cc")
 (require 'init-cc)
 
 ;; Configures c-mode
-(load-library "init-c")
 (require 'init-c)
 
 ;; Configures flycheck
-(load-library "init-flycheck")
 (require 'init-flycheck)
 
 ;; Configures Shell script mode
-(load-library "init-sh")
 (require 'init-sh)
 
 ;; Configures `erc' IRC client
-(load-library "init-erc")
 (require 'init-erc)
 
 ;; Configures `web-mode' 
-(load-library "init-web")
 (require 'init-web)
 
 ;; Configures `ansi-term' IRC client
-(load-library "init-term")
 (require 'init-term)
 
 ;; Configure specific files to be opened with specific modes.
-(load-library "init-files")
 (require 'init-files)
