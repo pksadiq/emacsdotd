3bb4c5a88f00494b2b9ab4475c9ef2e4f5711c99
Mohammed Sadiq <sadiq@sadiqpk.org>
2016-05-24 11:43:30 +0530

    org: don't include default scripts in html export

--- .emacs.d/personal/init-org.el
+++ .emacs.d/personal/init-org.el
@@ -3,6 +3,8 @@
 (require 'org)
 
 ;; Use HTML5 doctype for export
-(setq org-html-doctype "html5")
+(setq org-html-doctype "html5"
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil)
 
 (provide 'init-org)
