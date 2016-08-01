2ddec35e202550205c4c79f85f219abf13feaf37
Mohammed Sadiq <sadiq@sadiqpk.org>
2016-08-01 07:51:41 +0530

    org: don't include html w3 validation link

--- .emacs.d/personal/init-org.el
+++ .emacs.d/personal/init-org.el
@@ -5,6 +5,7 @@
 ;; Use HTML5 doctype for export
 (setq org-html-doctype "html5"
       org-html-head-include-scripts nil
-      org-html-head-include-default-style nil)
+      org-html-head-include-default-style nil
+      org-html-validation-link nil)
 
 (provide 'init-org)
