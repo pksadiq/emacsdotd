6877c8c0e5cd97bc4d8d2459d78b260b7e09a6f9
Mohammed Sadiq <sadiq@sadiqpk.org>
2016-08-10 20:47:56 +0530

    js: delete trailing whitespace on save

--- .emacs.d/personal/init-js.el
+++ .emacs.d/personal/init-js.el
@@ -6,5 +6,8 @@
 ;; indentation level for `js2-mode'
 (setq-default js2-basic-offset 2)
 
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
 (provide 'init-js)
