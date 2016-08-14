416c5b1d7113b1b4ba5b18d0675c094f5498985c
Mohammed Sadiq <sadiq@sadiqpk.org>
2016-08-14 18:42:09 +0530

    speedbar: open buffers initially

--- .emacs.d/personal/init-speedbar.el
+++ .emacs.d/personal/init-speedbar.el
@@ -3,7 +3,8 @@
 ;; Don't show images
 (setq
  speedbar-use-images nil
- sr-speedbar-width 23)
+ sr-speedbar-width 23
+ speedbar-initial-expansion-list-name "quick buffers")
 
 ;; reduce font size so that we require even less space
 (make-face 'speedbar-face)
