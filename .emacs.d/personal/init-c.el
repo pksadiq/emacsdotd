69535a817ec3c7fa682853bd451a95516a8467a4
Mohammed Sadiq <sadiq@sadiqpk.org>
2016-10-27 08:01:34 +0530

    c: fix macros to assume to have ';'

--- .emacs.d/personal/init-c.el
+++ .emacs.d/personal/init-c.el
@@ -5,13 +5,6 @@
 ;; Multiline comment prefix
 (setq c-block-comment-prefix "*")
 
-;; TODO: use regexp match
-(setq c-macro-names-with-semicolon
-      '("G_BEGIN_DECLS" "G_END_DECLS" "G_DECLARE_FINAL_TYPE"
-        "G_DECLARE_DERIVABLE_TYPE" "G_DECLARE_INTERFACE" "G_DEFINE_TYPE"
-        "G_DEFINE_TYPE_WITH_PRIVATE" "G_DEFINE_ABSTRACT_TYPE"
-        "G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE"))
-
 (defvar gnome-package nil)
 (make-variable-buffer-local 'gnome-package)
 
@@ -38,6 +31,14 @@
             (local-set-key (kbd "C-,") 'insert-comma)
             (local-set-key (kbd "C-{") 'insert-brace)
             (local-set-key (kbd "C-.") 'insert-period)
-            (electric-spacing-mode 1)))
+            (electric-spacing-mode 1)
+            ;; TODO: use regexp match
+            (setq c-macro-names-with-semicolon
+                  '("G_BEGIN_DECLS" "G_END_DECLS" "G_DECLARE_FINAL_TYPE"
+                    "G_DECLARE_DERIVABLE_TYPE" "G_DECLARE_INTERFACE" "G_DEFINE_TYPE"
+                    "G_DEFINE_TYPE_WITH_PRIVATE" "G_DEFINE_ABSTRACT_TYPE"
+                    "G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE"))
+
+))
 
 (provide 'init-c)
